270a8dc26f5231340c98c5f0669c49f673eacd02
tobon-marco <marco.tobon@email.ucr.edu>
2017-04-08 17:55:26 -0400

    === ADDED THE JOIN TABLE BEANS ===== - NOT TOO SURE IF THATS HOW YOU DO IT

---
+++ src/main/java/com/revature/kkoders/beans/GameLibraryImpl.java
@@ -0,0 +1,49 @@
+package com.revature.kkoders.beans;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name="GAME_LIBRARY")
+public class GameLibraryImpl implements GameLibrary
+{
+	@Id
+	private long id;
+	
+	@JoinTable
+	(
+		name = "GAME_LIBRARY",
+		joinColumns = @JoinColumn(name="user_id"),
+		inverseJoinColumns = @JoinColumn(name="game_id")
+	)
+	private List<GameImpl> games;
+
+	public final long getId()
+	{
+		return id;
+	}
+
+	public final void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public final List<GameImpl> getGames()
+	{
+		return games;
+	}
+
+	public final void setGames(List<GameImpl> games)
+	{
+		this.games = games;
+	}
+}
