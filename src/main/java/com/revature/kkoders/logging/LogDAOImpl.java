9654467fcc732a91e4376f43a5e0d4b6ccdc88ee
emhaechi <michael.a.hobbs@gmail.com>
2017-04-11 16:49:10 -0400

    controller edits for URL (#28)

---
+++ src/main/java/com/revature/kkoders/logging/LogDAOImpl.java
@@ -0,0 +1,12 @@
+package com.revature.kkoders.logging;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogDAOImpl implements LogDAO {
+
+	public void log() {
+		
+	}
+	
+}
